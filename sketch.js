b725827dbc00bdf6774cadcf3ef7b64f78d2ee74
casobri17 <casobri17@gmail.com>
2024-09-29 19:46:16 -0500

    test

---
+++ sketch.js
@@ -0,0 +1,38 @@
+let score = 0;
+let startTime;
+let gameTime = 15; //Game lasts for 15 seconds
+
+function setup()
+{
+    createCanvas(400,400);
+    startTime = millis(); //Records the time when the game starts
+}
+
+function draw()
+{
+    background(255);
+
+    //display score
+    textSize(24);
+    fill(0);
+    text('Score: ' + score, 10, 30); //10 and 30 are x and y coordinates. + is used to combine the string while score is the variable
+
+    //timer
+    let timeLeft = gameTime - int((millis() - startTime) / 1000); //millis - starttime calculates the total time that has elasped since the game started. /1000 converts milliseconds to seconds. int is integer
+    text('Time: ' + timeLeft, 300, 30);
+
+    //if time has run out
+    if (timeLeft <= 0) {
+        endGame();
+        return;
+    }
+}
+function endGame() {
+    background(255);
+    textSize(32);
+    fill(0);
+    textAlgn(CENTER, CENTER);
+    text('Game Over', width/2, height/2 - 20);
+    text('Final Score: ' + score, wdith/2, height/2 + 20);
+    noLoop(); //stops the game loop
+}
